40ff2f1c50b329a836ae6684f7a6c0e46cc1ac73
smartnet605 <softwaresmartnet@gmail.com>
2024-12-10 22:24:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
-const express=require('express')
-const app=express()
-const port=30000
-
-app.get('/get', (req, res)=>{
-    res.send('Hello World!')
-})
-
-app.listen(port,()=>{
-    console.log(`Example app listening on port ${port}`)
-})
+// Print "Hello, World!" to the console
+console.log("Hello, World!");
